2828136a8e0ba21265e53dc3eefd427a24191740
victorvaneecloo <victor.vaneecloo3@gmail.com>
2024-12-09 19:08:23 +0100

    feat: add new logic, graph parsing etc.

--- macros/_get_merge_query.sql
+++ macros/_get_merge_query.sql
@@ -1,79 +1,102 @@
-{% macro _get_merge_query(model, start_run, end_run, batch_size, event_time) %}
-    {% set merge_query = "" %}
-    {% for event_start, event_end in _get_interval_logic(start_run=start_run, end_run=end_run, event_time=event_time).items() %}
-        {% set merge_query = merge_query ~ _format_query_template(model=model, event_start=event_start, event_end=event_end, event_time=event_time) %}
+{% macro _get_merge_query(model, start_run, end_run, batch_size, date_column) %}
+    {% set merge_queries = [] %}
+    {% for event_start, event_end in _get_interval_logic(start_run=start_run, end_run=end_run, batch_size=batch_size).items() %}
+        {{ merge_queries.append(_format_query_template(model=model, event_start=event_start, event_end=event_end, date_column=date_column)) }}
     {% endfor %}
 
-    {{ return(merge_query) }}
+    {{ return("".join(merge_queries)) }}
 {% endmacro %}
 
-{% macro _get_interval_logic(start_run, end_run, batch_size, event_time) %}
-    {% set datetime = modules.datetime.datetime %}
-    {% set event_time_dict = {
+{% macro _get_interval_logic(start_run, end_run, batch_size) %}
+    {% set datetime = modules.datetime %}
+    {% set events = {} %}
+    {% set batch_size_dict = {
         "day": 1,
         "week": 7,
         "month": 30,
         "year": 365
     } %}
 
-    {% set start_run_date = datetime.date(start_run) %}
-    {% set end_run_date = datetime.date(end_run) %}
+    {% set step_interval = batch_size_dict.get(batch_size, 0) %}
 
-    {% set events = {} %}
-    {% set tmp_start_run = start_run_date %}
-   
-    {% while tmp_start_run > end_run_date %}
-        {% set tmp_end_run = tmp_start_run - datetime.timedelta(days=event_time_dict[event_time]) %}
-        {% set events = events | merge({tmp_start_run: tmp_end_run}) %}
-        {% set tmp_start_run = tmp_end_run %}
-    {% endwhile %}
+    {% set days_diff = (start_run - end_run).days | int %}
+    {% set steps = range(0, days_diff, step_interval) %}
+
+
+    {% for step in steps %}
+        {% set tmp_start_run = start_run - datetime.timedelta(days=step) %}
+        {% set tmp_end_run = tmp_start_run - datetime.timedelta(days=step_interval) %}
+        {% do events.update({tmp_start_run: tmp_end_run}) %}
+    {% endfor %}
 
     {{ return(events) }}
 {% endmacro %}
 
-
-{% macro _format_query_template(model, event_start, event_end, event_time, materialization_type="merge") %}
+{% macro _format_query_template(model, event_start, event_end, date_column, materialization_type="merge") %}
     {%- if materialization_type == "merge" -%}
+        {% set clean_code = _get_clean_code(model=model) %}
+        {{ print("clean code" ~ clean_code) }}
         {% set merge_query_template %}
-            merge with schema evolution into {{ target }}.{{ model }}
-            using (
-                select
-                    *
-                from
-                    {{ ref(model)}}
-                where
-                    {{ event_time }} is between {{ event_start }} and {{ event_end }}
-            )
-            on source.event_time = target.event_time
-            when matched then
-                update set *
-            when not matched then
-                update set *
-            when not matched by source then
-                delete;
+        merge with schema evolution into {{ ref(model) }} as target
+                using (
+                with source as ({{ clean_code }})
+                    select
+                        *
+                    from
+                        source
+                    where
+                        {{ date_column }} between date({{ event_end }}) and date({{ event_start }})
+                ) as source
+                on source.created_at = target.created_at
+                when matched then
+                    update set *
+                when not matched then
+                    insert *
+                when not matched by source then
+                    delete;
         {% endset %}
     {%- endif -%}
+    {{ return(merge_query_template) }}
+{% endmacro %}
 
-    {%- if materialization_type == "insert_overwrite" -%}
-        {# TBD #}
-        {% set merge_query_template %}
-            merge with schema evolution into {{ target }}.{{ model }}
-            using (
-                select
-                    *
-                from
-                    {{ ref(model)}}
-                where
-                    {{ event_time }} is between {{ event_start }} and {{ event_end }}
-            )
-            on source.event_time = target.event_time
-            when matched then
-                update set *
-            when not matched then
-                update set *
-            when not matched by source then
-                delete;
-        {% endset %}
-    {%- endif -%}
-    {{ do return(merge_query_template) }}
+
+{% macro _get_clean_code(model) %}
+    {{ return(_compile(_clean_raw_code(_get_raw_code(model)))) }}
+{% endmacro %}
+
+{% macro _clean_raw_code(raw_code) %}
+    {%- set lower_raw_code = raw_code | lower -%}
+    {%- set code_without_config = lower_raw_code.split(")\n}}") -%}
+    {%- set cleaned_code = code_without_config[1].split("{{ row_limit_for_ci() }}") -%}
+    {{ return(cleaned_code[0]) }}
+{% endmacro %}
+
+{% macro _get_raw_code(model) %}
+    {% if execute %}
+        {% for node in graph.nodes.values()
+         | selectattr("resource_type", "equalto", "model")
+         | selectattr("package_name", "equalto", "insight_supply_chain")
+         | selectattr("name", "equalto", model) %}
+            {{ return(node.raw_code) }}
+        {%- endfor -%}
+    {% else %}
+        {{ exceptions.raise_compiler_error("[ERROR] DBT is not executing, cannot parse graph.") }}
+    {% endif %}
+{% endmacro %}
+
+{% macro _compile(raw_code) %}
+    {% set re = modules.re %}
+    {%- set models = re.findall('{{\s*ref\((.*?)\)\s*}}', raw_code) %}
+    {%- set compiled_code = raw_code %}
+
+    {%- for model in models -%}
+        {% set compiled_ref = ref(model) %}
+
+        {# Dynamically build the regex pattern to match `{{ ref('') }}` with the model name #}
+        {% set regex_pattern = '{{\s*ref\((.*?)\)\s*}}' %}
+
+        {# Use re.sub to replace the found `{{ ref() }}` with the compiled reference #}
+        {% set compiled_code = re.sub(regex_pattern, compiled_ref | string, compiled_code) %}
+        {{ print(compiled_code) }}
+    {%- endfor -%}
 {% endmacro %}
--- macros/backfill.sql
+++ macros/backfill.sql
@@ -1,33 +1,34 @@
-{% macro backfill(model=None, start_run=None, end_run=None, batch_size=None, event_time=None, debug=False) %}
-    {{ log("Running the backfill macro from" ~ start_run ~  "to" ~ end_run ~ "with a batch size of " ~ event_time}}
-    {{ _catch_var_edge_cases(model, start_run, end_run, batch_size, event_time, debug) }}
-    {{ do run_query(_get_merge_query(model, start_run, end_run, batch_size, event_time, debug)) }}
+{% macro new_backfill(model=None, start_run=None, end_run=None, batch_size="day", date_column=None) %}
+    {{ _log_and_print("[INFO] Running the backfill macro from " ~ start_run ~  " to " ~ end_run ~ " with a batch size of " ~ batch_size ~ ".") }}
+
+    {{ _catch_var_edge_cases(model, start_run, end_run, batch_size, date_column) }}
+    {{ run_query(_get_merge_query(model, start_run, end_run, batch_size, date_column)) }}
 {% endmacro %}
 
 
-{% macro _catch_edge_cases(model, start_run, end_run, batch_size, event_time, debug) %}
+{% macro _catch_var_edge_cases(model, start_run, end_run, batch_size, date_column) %}
     {%- if not model -%}
-        {{ exceptions.raise_compiler_error("[ERROR] The model var is neither specified in the backfill vars nor in the model config. Got:" ~ model ~ ".") }}
+        {{ exceptions.raise_compiler_error("[ERROR] The model var is not specified in the backfill vars. Got: " ~ model ~ ".") }}
     {%- endif -%}
 
     {%- if not start_run -%}
-        {{ exceptions.raise_compiler_error("[ERROR] The start_run var is neither specified in the backfill vars nor in the model config. Got:" ~ start_run ~ ".") }}
+        {{ exceptions.raise_compiler_error("[ERROR] The start_run var is not specified in the backfill vars. Got: " ~ start_run ~ ".") }}
     {%- endif -%}
 
     {%- if not end_run -%}
-        {{ exceptions.raise_compiler_error("[ERROR] The end_run var is neither specified in the backfill vars nor in the model config. Got:" ~ end_run ~ ".") }}
+        {{ exceptions.raise_compiler_error("[ERROR] The end_run var is not specified in the backfill vars. Got: " ~ end_run ~ ".") }}
     {%- endif -%}
 
     {%- if not batch_size -%}
-        {{ exceptions.warn("[WARN] The batch_size var is neither specified in the backfill vars nor in the model config. Got:" ~ batch_size ~ ". The backfill will begin with a 'batch_size' of 1 day.") }}
-        {% set batch_size = "day" %}
+        {{ exceptions.warn("[WARN] The batch_size var is not specified in the backfill vars. Got: " ~ batch_size ~ ". The backfill will begin with a 'batch_size' of 1 day.") }}
     {%- endif -%}
 
-    {%- if not event_time -%}
-        {{ exceptions.raise_compiler_error("[ERROR] The event_time var is neither specified in the backfill vars nor in the model config. Got:" ~ event_time ~ ".") }}
+    {%- if not date_column -%}
+        {{ exceptions.raise_compiler_error("[ERROR] The date_column var is not specified in the backfill vars. Got: " ~ date_column ~ ".") }}
     {%- endif -%}
+{% endmacro %}
 
-    {%- if debug -%}
-        {{ exceptions.raise_compiler_error("[ERROR] The event_time var is neither specified in the backfill vars nor in the model config. Got:" ~ event_time ~ ".") }}
-    {%- endif -%}
+{% macro _log_and_print(message) %}
+    {{ log(message) }}
+    {{ print(message) }}
 {% endmacro %}
